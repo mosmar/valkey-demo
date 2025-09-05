e888cc2bee9a2e0a8372081abb854928054ed4ee
Micca Osmar <miccaosmar@macbookpro.lan>
2025-09-04 23:42:44 -0400

    Added additional commands

--- examples/basic operations/commands.sh
+++ examples/basic operations/commands.sh
@@ -2,22 +2,29 @@ valkey-cli ping
 
 # key
 valkey-cli set mykey "hello"
-
 valkey-cli get mykey
 
 # string: The simplest data type in Valkey is a string
 valkey-cli SET user:1000 "mosmar"
-
 valkey-cli GET user:1000
-
 valkey-cli DEL user:1000
 
 # Hashes: Hashes allow you to store multiple fields and values under one key, similar to a JSON object or dictionary
 valkey-cli HSET user:1000 name "Alice" email "alice@example.com" age "30"
-
 valkey-cli HGET user:1000 name
-
 valkey-cli HGETALL user:1000
-
 valkey-cli DEL user:1000
 
+# Sets
+valkey-cli SADD colors red blue green
+valkey-cli SMEMBERS colors
+
+# Lists
+valkey-cli LPUSH tasks "task1"
+valkey-cli LPUSH tasks "task2"
+valkey-cli LRANGE tasks 0 -1
+
+# Increment number stored by key
+valkey-cli INCR player:42:points
+valkey-cli INCR player:42:points
+valkey-cli GET player:42:points
