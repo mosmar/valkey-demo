5ff92423b61ca794b94d11a54612a07d039fee50
Micca Osmar <miccaosmar@macbookpro.lan>
2025-09-04 23:43:57 -0400

    Benchmarks can now be run in docker container

--- examples/benchmark/benchmark.sh
+++ examples/benchmark/benchmark.sh
@@ -1,1 +1,10 @@
+# Run client benchmark if Valkey installed locally
 valkey-cli valkey-benchmark -n 10000 -c 50
+
+# run it client benchmark inside a separate container
+
+# Valkey
+docker run --rm valkey/valkey valkey-benchmark -h host.docker.internal -p 6380 -n 100000 -c 50
+
+# Redis
+docker run --rm redis redis-benchmark -h host.docker.internal -p 6379 -n 100000 -c 50
